e18227074efa1a6e5b62ecbe1f63e29551139b95
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-30 09:34:26 -0500

    created picTruck

--- Depreciation/Depreciation/frmDisplayInventory.Designer.vb
+++ Depreciation/Depreciation/frmDisplayInventory.Designer.vb
@@ -23,6 +23,8 @@ Partial Class frmDisplayInventory
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.lblTitle2 = New System.Windows.Forms.Label()
+        Me.picTruck = New System.Windows.Forms.PictureBox()
+        CType(Me.picTruck, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'lblTitle2
@@ -30,11 +32,22 @@ Partial Class frmDisplayInventory
         Me.lblTitle2.AutoSize = True
         Me.lblTitle2.Font = New System.Drawing.Font("Microsoft Sans Serif", 17.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lblTitle2.ForeColor = System.Drawing.Color.OrangeRed
-        Me.lblTitle2.Location = New System.Drawing.Point(70, 9)
+        Me.lblTitle2.Location = New System.Drawing.Point(84, 13)
         Me.lblTitle2.Name = "lblTitle2"
         Me.lblTitle2.Size = New System.Drawing.Size(230, 29)
         Me.lblTitle2.TabIndex = 0
         Me.lblTitle2.Text = "Chef Stack Inventory"
+        '
+        'picTruck
+        '
+        Me.picTruck.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.picTruck.Image = Global.Depreciation.My.Resources.Resources.truck
+        Me.picTruck.Location = New System.Drawing.Point(0, 0)
+        Me.picTruck.Name = "picTruck"
+        Me.picTruck.Size = New System.Drawing.Size(79, 59)
+        Me.picTruck.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picTruck.TabIndex = 1
+        Me.picTruck.TabStop = False
         '
         'frmDisplayInventory
         '
@@ -42,13 +55,16 @@ Partial Class frmDisplayInventory
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(371, 380)
+        Me.Controls.Add(Me.picTruck)
         Me.Controls.Add(Me.lblTitle2)
         Me.Name = "frmDisplayInventory"
         Me.Text = "Sorted Inventory Listing"
+        CType(Me.picTruck, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents lblTitle2 As Label
+    Friend WithEvents picTruck As PictureBox
 End Class
