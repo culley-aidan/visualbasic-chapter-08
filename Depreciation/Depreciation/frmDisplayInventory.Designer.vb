c58d85c867a6e99be1b0437439f7c789a176c7bb
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-30 09:37:22 -0500

    changed AcceptButton to btnReturn

--- Depreciation/Depreciation/frmDisplayInventory.Designer.vb
+++ Depreciation/Depreciation/frmDisplayInventory.Designer.vb
@@ -77,6 +77,7 @@ Partial Class frmDisplayInventory
         '
         'frmDisplayInventory
         '
+        Me.AcceptButton = Me.btnReturn
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
