d67ef78e8db6a0722d892c4044d5da93873b39db
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-30 09:39:42 -0500

    began the code

--- Depreciation/Depreciation/Form1.vb
+++ Depreciation/Depreciation/Form1.vb
@@ -1,3 +1,11 @@
-﻿Public Class frmDepreciation
+﻿' Program Name: Chef Shack Food Truck Depreciation Windows Application
+' Author:       Aidan J Culley
+' Date:         September 30th, 2019
+' Purpose:      The Chef Shack Inventory Windows Application determines
+'               the depreciation based on a 5 year life of items in inventory
+'               using the straight-line and double-declining balance methods.
+
+
+Public Class frmDepreciation
 
 End Class
