31bb61984cc8fb84e02e66f3a2d08698e91c5c83
culley-aidan <culley-aidan@users.noreply.github.com>
2019-10-01 08:35:55 -0500

    finished frmLoad event

--- Depreciation/Depreciation/frmDisplayInventory.vb
+++ Depreciation/Depreciation/frmDisplayInventory.vb
@@ -3,4 +3,18 @@
 
 Option Strict On
 Public Class frmDisplayInventory
+    Private Sub FrmDisplayInventory_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' the frmDisplayInventory load event is a second form that 
+        ' displays the sorted inventory items 
+
+        Dim strItem As String
+
+        ' Sorts the _strInventoryItem array 
+        Array.Sort(frmDepreciation._strInventoryItem)
+
+        ' Displays the _strInventoryItem array
+        For Each strItem In frmDepreciation._strInventoryItem
+            lstDisplay.Items.Add(strItem)
+        Next
+    End Sub
 End Class
