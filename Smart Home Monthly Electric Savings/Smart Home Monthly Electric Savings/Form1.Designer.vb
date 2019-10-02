954c756dcc80ed5a95b499d501f0928d1f90457a
culley-aidan <culley-aidan@users.noreply.github.com>
2019-10-02 08:13:39 -0500

    added picSmartHome

--- Smart Home Monthly Electric Savings/Smart Home Monthly Electric Savings/Form1.Designer.vb
+++ Smart Home Monthly Electric Savings/Smart Home Monthly Electric Savings/Form1.Designer.vb
@@ -22,17 +22,32 @@ Partial Class frmSmartHome
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.picSmartHome = New System.Windows.Forms.PictureBox()
+        CType(Me.picSmartHome, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'picSmartHome
+        '
+        Me.picSmartHome.Image = Global.Smart_Home_Monthly_Electric_Savings.My.Resources.Resources.smarthome
+        Me.picSmartHome.Location = New System.Drawing.Point(0, 1)
+        Me.picSmartHome.Name = "picSmartHome"
+        Me.picSmartHome.Size = New System.Drawing.Size(253, 217)
+        Me.picSmartHome.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picSmartHome.TabIndex = 0
+        Me.picSmartHome.TabStop = False
         '
         'frmSmartHome
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(540, 450)
+        Me.Controls.Add(Me.picSmartHome)
         Me.Name = "frmSmartHome"
         Me.Text = "Smart Home Application"
+        CType(Me.picSmartHome, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents picSmartHome As PictureBox
 End Class
