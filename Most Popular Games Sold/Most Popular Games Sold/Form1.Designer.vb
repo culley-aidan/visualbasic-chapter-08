2385453c0cd0f013c21785819ec5b0f153355d55
culley-aidan <culley-aidan@users.noreply.github.com>
2019-10-04 09:41:37 -0500

    Update Form1.Designer.vb

--- Most Popular Games Sold/Most Popular Games Sold/Form1.Designer.vb
+++ Most Popular Games Sold/Most Popular Games Sold/Form1.Designer.vb
@@ -26,6 +26,8 @@ Partial Class frmGames
         Me.lstGames = New System.Windows.Forms.ListBox()
         Me.cboSelectGame = New System.Windows.Forms.ComboBox()
         Me.lblDownloads = New System.Windows.Forms.Label()
+        Me.lblGameDownloads = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'lblTitle
@@ -43,7 +45,7 @@ Partial Class frmGames
         Me.lstGames.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lstGames.FormattingEnabled = True
         Me.lstGames.ItemHeight = 24
-        Me.lstGames.Location = New System.Drawing.Point(18, 57)
+        Me.lstGames.Location = New System.Drawing.Point(18, 76)
         Me.lstGames.Name = "lstGames"
         Me.lstGames.Size = New System.Drawing.Size(348, 244)
         Me.lstGames.TabIndex = 1
@@ -52,7 +54,7 @@ Partial Class frmGames
         '
         Me.cboSelectGame.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.cboSelectGame.FormattingEnabled = True
-        Me.cboSelectGame.Location = New System.Drawing.Point(501, 84)
+        Me.cboSelectGame.Location = New System.Drawing.Point(440, 96)
         Me.cboSelectGame.Name = "cboSelectGame"
         Me.cboSelectGame.Size = New System.Drawing.Size(200, 32)
         Me.cboSelectGame.TabIndex = 2
@@ -61,17 +63,39 @@ Partial Class frmGames
         '
         Me.lblDownloads.AutoSize = True
         Me.lblDownloads.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblDownloads.Location = New System.Drawing.Point(529, 57)
+        Me.lblDownloads.Location = New System.Drawing.Point(472, 57)
         Me.lblDownloads.Name = "lblDownloads"
         Me.lblDownloads.Size = New System.Drawing.Size(152, 24)
         Me.lblDownloads.TabIndex = 3
         Me.lblDownloads.Text = "Find Downloads "
         '
+        'lblGameDownloads
+        '
+        Me.lblGameDownloads.AutoSize = True
+        Me.lblGameDownloads.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGameDownloads.Location = New System.Drawing.Point(450, 152)
+        Me.lblGameDownloads.Name = "lblGameDownloads"
+        Me.lblGameDownloads.Size = New System.Drawing.Size(174, 40)
+        Me.lblGameDownloads.TabIndex = 4
+        Me.lblGameDownloads.Text = "$GAME has had" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "$DOWNLOADS total"
+        Me.lblGameDownloads.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(440, 255)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(194, 46)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Calculate All Downloads"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'frmGames
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(689, 352)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lblGameDownloads)
         Me.Controls.Add(Me.lblDownloads)
         Me.Controls.Add(Me.cboSelectGame)
         Me.Controls.Add(Me.lstGames)
@@ -87,4 +111,6 @@ Partial Class frmGames
     Friend WithEvents lstGames As ListBox
     Friend WithEvents cboSelectGame As ComboBox
     Friend WithEvents lblDownloads As Label
+    Friend WithEvents lblGameDownloads As Label
+    Friend WithEvents Button1 As Button
 End Class
